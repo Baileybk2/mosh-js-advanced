03357abb40cac1445402d18c6509e030635bc744
BaileyBeck <baileybk2@gmail.com>
2024-11-08 15:56:09 -0500

    prototypical inheritance exercise

--- index2.js
+++ index2.js
@@ -119,3 +119,30 @@ mixin(Goldfish.prototype, canEat, canSwim)
 
 const goldFish = new Goldfish()
 console.log(goldFish)
+
+// Exercise: Prototypical Inheritance
+
+function HtmlElement() {
+  this.click = function () {
+    console.log("clicked")
+  }
+}
+
+HtmlElement.prototype.focus = function () {
+  console.log("focused")
+}
+
+function HtmlSelectElement(items = []) {
+  this.items = items
+
+  this.addItem = function (item) {
+    this.items.push(item)
+  }
+
+  this.removeItem = function (item) {
+    this.items.splice(this.items.indexOf(item), 1)
+  }
+}
+
+HtmlSelectElement.prototype = new HtmlElement()
+HtmlSelectElement.prototype.constructor = HtmlSelectElement()
